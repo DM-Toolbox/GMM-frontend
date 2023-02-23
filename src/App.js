dd311c993bf3e90082d81cda3e8ab5cd2bd00365
GeoSauer <geoffrey.sauer89@gmail.com>
2023-02-23 12:58:18 -0700

    adds routing and general layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import AboutPage from './components/AboutPage/AboutPage';
 import LandingPage from './components/LandingPage/LandingPage';
+import Layout from './components/PageLayout/Layout';
+import ProfilePage from './components/ProfilePage/ProfilePage';
+import ProtectedRoute from './components/LandingPage/ProtectedRoutes';
 import SignInForm from './components/LandingPage/SignInForm';
 import SignUpForm from './components/LandingPage/SignUpForm';
+import SpellList from './components/SpellListPage/SpellList';
 import Welcome from './components/LandingPage/Welcome';
 
 function App() {
@@ -14,6 +19,18 @@ function App() {
         <Route path="signup" element={<SignUpForm />} />
         <Route path="signin" element={<SignInForm />} />
       </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route element={<Layout />}>
+          {/* <Route index element={<TBD />} /> */}
+          <Route path="spells" element={<SpellList />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="about" element={<AboutPage />} />
+        </Route>
+      </Route>
+      <Route
+        path="*"
+        element={<Navigate to="spells" replace />}
+      />
     </Routes>
   );
 }
--- src/components/PageLayout/Layout.jsx
+++ src/components/PageLayout/Layout.jsx
@@ -1,5 +1,20 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 import './Layout.css';
+import { useUser } from '../../context/UserContext';
 
 export default function Layout() {
-  return <div>Layout</div>;
+  const user = useUser();
+  console.log(user);
+  if (!user) return <Navigate to="welcome" />;
+  return (
+    <div className="Layout">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
